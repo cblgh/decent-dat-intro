2486a72de96db6bbb9c7f24cdb663a4285bc3ad3
Alexander Cobleigh <cblgh@cblgh.org>
2018-02-27 12:08:09 +0100

    improve the simple chat app to a mvp kinda level :3

--- the-simple-chat-1/script.js
+++ the-simple-chat-1/script.js
@@ -1,19 +1,83 @@
 var messages = []
+var peers = []
 
 document.addEventListener("DOMContentLoaded", function() {
     readChat(window.location.host)
+    loadFriends()
 })
 
+function loadFriends() {
+    var archive = new DatArchive(window.location.host)
+    archive.readFile("chat.json").then((contents) => {
+        var chat = JSON.parse(contents)
+        chat.friends.map(readChat)
+    })
+}
+
 function readRemote() {
-    var peer = document.getElementById("peer").value
+    var peerInput = document.getElementById("peer")
+    var peer = peerInput.value
+    peerInput.value = ""
     readChat(peer)
+    
 }
 
 function appendNewPeer(peer) {
+    peer = normalize(peer)
+    if (peers.indexOf(peer) >= 0) {
+        return
+    }
+    peers.push(peer)
     var peerList = document.getElementById("peers")
     var newPeer = document.createElement("li")
     newPeer.textContent = peer
     peerList.appendChild(newPeer)
+
+    var archive = new DatArchive(window.location.host)
+    return archive.readFile("chat.json").then((contents) => {
+        var chat = JSON.parse(contents)
+        if (chat.friends.indexOf(peer) < 0 && peer !== window.location.host) {
+            chat.friends.push(peer)
+        }
+        return archive.writeFile("chat.json", JSON.stringify(chat, null, 2))
+    })
+}
+
+// thx to 0xade & rotonde for this wonderful function <3
+function normalize(url)
+{
+  if (url && url.url)
+    url = url.url;
+  if (!url)
+    return null;
+
+  // This is microoptimized heavily because it's called often.
+  // "Make slow things fast" applies here, but not literally:
+  // "Make medium-fast things being called very often even faster."
+  
+  if (
+    url.length > 6 &&
+    url[0] == 'd' && url[1] == 'a' && url[2] == 't' && url[3] == ':'
+  )
+    // We check if length > 6 but remove 4.
+    // The other 2 will be removed below.
+    url = url.substring(4);
+  
+  if (
+    url.length > 2 &&
+    url[0] == '/' && url[1] == '/'
+  )
+    url = url.substring(2);
+
+  var index = url.indexOf("/");
+  url = index == -1 ? url : url.substring(0, index);
+
+  url = url.toLowerCase().trim();
+  return url;
+}
+
+function compare(a, b) {
+    return parseInt(a.time) > parseInt(b.time)
 }
 
 function readChat(dat) {
@@ -42,6 +106,7 @@ function readChat(dat) {
                 return true
             })
             messages = messages.concat(newMessages)
+            messages.sort(compare)
             renderMessages()
         })
     }
